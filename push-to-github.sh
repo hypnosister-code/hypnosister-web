fdd5a41ec5832d6de6586acfeb861fe025a1fea0
hypnosister-code <cchelikopter@gmail.com>
2025-05-02 19:52:00 +0200

    Aktualizacja stylu, sidebarów i wpisów na blogu

--- push-to-github.sh
+++ push-to-github.sh
@@ -5,6 +5,6 @@ git add .
 git commit -m "Aktualizacja stylu, sidebarów i wpisów na blogu"
 git branch | grep -q 'main' && BRANCH=main || BRANCH=master
 
-git remote | grep -q origin || git remote add origin https://github.com/cchelikopter/hypnosister-code.git
+git remote | grep -q origin || git remote add origin https://github.com/hypnosister-code/hypnosister-web.git
 
 git push origin $BRANCH
